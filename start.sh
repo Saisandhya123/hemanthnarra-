e232df7cc2cc9738ca3736670f1c0fd1506af543
Sai sandhya yadav <87059170+Saisandhya123@users.noreply.github.com>
2021-07-07 12:41:04 +0530

    Initial commit

---
+++ start.sh
@@ -0,0 +1,7 @@
+app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
+do
+    sleep 0.1
+done
+echo Tailscale started
+ALL_PROXY=socks5://localhost:1055/ /app/my-app
